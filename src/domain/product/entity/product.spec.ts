ae98e29893bb06b8a42f782e040fe4771d055577
Pedro Furtado <pedro.furtado@locaweb.com.br>
2024-04-02 11:49:41 -0300

    notification pattern para product

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -16,7 +16,13 @@ describe("Product unit tests", () => {
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("123", "Name", -1);
-    }).toThrowError("Price must be greater than zero");
+    }).toThrowError("product: Price must be greater than zero");
+  });
+
+  it("should throw error when multiple validations fails", () => {
+    expect(() => {
+      const product = new Product("product A", "", -56);
+    }).toThrowError("product: Name is required,product: Price must be greater than zero");
   });
 
   it("should change name", () => {
